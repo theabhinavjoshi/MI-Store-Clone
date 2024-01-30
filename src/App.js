31e459064398eb60292bd66ab42ef6cbbf24efc8
theabhinavjoshi <jabhinav2014@gmail.com>
2024-01-30 23:18:12 +0545

    Added new components and respective styles

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import './App.css';
 import PreNavbar from './components/PreNavbar';
 import Navbar from "./components/Navbar.js";
 import {BrowserRouter as Router} from "react-router-dom";
+import Slider from "./components/Slider.js";
+import data from "./data/data.json";
+import Offers from "./components/Offers.js";
+import Heading from "./components/Heading.js"
+import StarProduct from "./components/StarProduct.js"
 
 function App() {
   return (
@@ -9,6 +14,11 @@ function App() {
       
       <PreNavbar />
       <Navbar />
+      <Slider start={data.banner.start} />
+      <Offers offer={data.offer}/>
+      <Heading text="STAR PRODUCTS" />
+      <StarProduct starProduct={data.starProduct} />
+      <Heading text="HOT ACCESSORIES" />
 
     </Router>
   );
